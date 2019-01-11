bd93e5ba1fa031f9e2bc6e2d94ecfe3e51694bbb
Syd Amir <hemedani@gmail.com>
2019-01-11 10:57:14 +0330

    add redux persist and some styling

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import TodoPromodo from "./src/TodoPromodo";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
 import { store } from "./src/configureStore";
+import { persistor } from "./src/configureStore";
 export default class App extends React.PureComponent {
   render() {
     return (
       <Provider store={store}>
-        <TodoPromodo />
+        <PersistGate loading={null} persistor={persistor}>
+          <TodoPromodo />
+        </PersistGate>
       </Provider>
     );
   }
--- src/components/Todo/AddTodo.js
+++ src/components/Todo/AddTodo.js
@@ -1,33 +1,52 @@
 import React from "react";
 import { Modal, View, StyleSheet, Alert } from "react-native";
 import { Formik, Field } from "formik";
-import { Button } from "react-native-elements";
+import { Button, Header } from "react-native-elements";
 import CustomInp from "../../utils/CustomInp";
 
 const AddTodo = ({ addModalVisibilty, toggleAddTodoModal, addTodo }) => (
   <Modal animationType="slide" transparent={false} visible={addModalVisibilty}>
-    <View style={styles.container}>
+    <Header centerComponent={{ text: "Adding a Todo", style: { color: "#fff" } }} />
+    <View>
       <Formik
         onSubmit={({ title, description }) => {
           addTodo({ title, description });
+          toggleAddTodoModal();
         }}
         render={({ handleSubmit }) => (
           <View>
             <Field component={CustomInp} name="title" />
             <Field component={CustomInp} name="description" />
-            <Button raised icon={{ name: "add" }} title="Add Todo" onPress={handleSubmit} />
+            <View style={styles.btnContainer}>
+              <Button
+                title="Add Todo"
+                raised
+                borderRadius={8}
+                icon={{ name: "add" }}
+                backgroundColor="#00BB5E"
+                onPress={handleSubmit}
+              />
+              <Button
+                title="Close Modal"
+                raised
+                borderRadius={8}
+                icon={{ name: "close" }}
+                backgroundColor="#E5000F"
+                onPress={toggleAddTodoModal}
+              />
+            </View>
           </View>
         )}
       />
-
-      <Button raised icon={{ name: "close" }} title="Close Modal" onPress={toggleAddTodoModal} />
     </View>
   </Modal>
 );
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: 22
+  btnContainer: {
+    marginTop: 8,
+    flexDirection: "row",
+    justifyContent: "space-evenly"
   }
 });
 
